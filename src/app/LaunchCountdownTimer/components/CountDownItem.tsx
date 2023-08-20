ac962c8a3fed969f85fce128160ed14020ff64d9
Nikoditten <48121115+Nikditten@users.noreply.github.com>
2023-08-20 12:55:44 +0200

    Update CountDownItem.tsx

--- src/app/LaunchCountdownTimer/components/CountDownItem.tsx
+++ src/app/LaunchCountdownTimer/components/CountDownItem.tsx
@@ -18,10 +18,10 @@ const CountDownItem: FC<Props> = ({ value, label }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <div className="h-24 w-24 relative rounded-lg bg-gradient-to-b from-[#1e1f29] to-[#343650]">
-        <h1 className="w-full pt-[5px] absolute top-1/2 -translate-y-1/2 font-bold text-[#fb6087] text-4xl text-center">
+        <h1 className="w-full absolute top-1/2 -translate-y-1/2 font-bold text-[#fb6087] text-4xl text-center">
           {valueFormatted()}
         </h1>
-        <div className="h-full w-full absolute flex flex-row justify-center items-center">
+        <div className="w-full top-1/2 -translate-y-1/2 absolute flex flex-row justify-center items-center">
           <div className="h-2 w-1 bg-[#191a24] rounded-tr-full rounded-br-full" />
           <div className="h-[0.5px] w-full bg-[#191a24]" />
           <div className="h-2 w-1 bg-[#191a24] rounded-tl-full rounded-bl-full" />
